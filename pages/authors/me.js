328caf726a2df56aef546c1feb71f49d177bd83a
Szhan118 <46965816+Szhan118@users.noreply.github.com>
2021-12-17 18:33:55 -0500

    first commit

---
+++ pages/authors/me.js
@@ -0,0 +1,5 @@
+export default function FirstPost() {
+    const name = "Avrilllllll"
+
+    return <h1>{name}</h1>
+  }
---
+++ pages/index.js
@@ -0,0 +1,48 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+import Card from '../components/card'
+import Col from '../components/col'
+import Container from '../components/container'
+import Layout from '../components/layout'
+import Row from '../components/row'
+
+import { getAllMovies } from '../lib/api'
+
+export async function getStaticProps() {
+
+  const movies = await getAllMovies();
+
+  return {
+    props : {
+      movies
+    }
+  }
+}
+
+export default function Home({ movies }) {
+  //console.log({movies});
+  return (
+    <Layout>
+      <Head>
+        <title>IST 363 Netflix</title>
+      </Head>
+      <main>
+        <Container>
+          <Row>
+            {movies.nodes.map((movie, index) => {
+              const {title, slug, featuredImage} = movie;
+              return <Col key = {index} xs="6" sm="6" md="4" lg="3">
+                <Card 
+                  title = {title}
+                  featuredImage = {featuredImage}
+                  slug = {slug}
+                />
+              </Col>
+            })}
+          </Row>
+        </Container>
+      </main>
+    </Layout>
+  )
+  }
